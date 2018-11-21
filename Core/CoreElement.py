ddfc53f93288f185ea7bfcdf1839d89fbb97e954
Iceyo Kuna <iceyokuna@hotmail.com>
2018-11-21 14:45:55 +0700

    Initialize project

---
+++ Core/CoreElement.py
@@ -0,0 +1,4 @@
+class CoreElement:
+    def __init__(self, elementId, elementName):
+        self.id = elementId
+        self.name = elementName
---
+++ Core/Task.py
@@ -0,0 +1,11 @@
+class Task(CoreElement):
+    def __init__(self , task_id, task_name, ):
+        super.__init__(task_id, task_name)
+        self.service_obj = None
+        self.flow = None
+
+    def performService(self):
+        pass
+
+
+    
